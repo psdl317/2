9ab930912c28412cc094d581f8a8230c81b1e005
Yuvraj1819 <yuvrajsahu1819@gmail.com>
2025-04-28 23:27:12 +0530

    Create asc.c

---
+++ asc.c
@@ -0,0 +1,31 @@
+#include <xc.h>
+
+
+void main(void) {
+    int i, j, t;
+    int a[] = {0x01, 0x03, 0x06, 0x04, 0x07, 0x02, 0x05, 0x08};
+
+    
+    for (i = 0; i < 7; i++) {
+        for (j = 0; j < 7 - i; j++) {
+            if (a[j] > a[j + 1]) {
+                t = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = t;
+            }
+        }
+    }
+
+    TRISD = 0; 
+
+    while (1) {
+        for (i = 0; i < 8; i++) {
+            PORTD = a[i];
+            for (int n = 0; n < 1000; n++) {
+                for (int k = 0; k < 100; k++) {
+                    
+                }
+            }
+        }
+    }
+}
